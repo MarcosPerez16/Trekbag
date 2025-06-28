851338fc40580866b5c8533f24d67df181e51aec
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-06-27 20:28:59 -0700

    added conditional rendering logic for when the list is empty

---
+++ src/components/EmptyView.jsx
@@ -0,0 +1,10 @@
+const EmptyView = () => {
+  return (
+    <section className='empty__list-text'>
+      <h3>Empty Packing List</h3>
+      <p>Start by adding some items you absolutley don&apos;t want to forget</p>
+    </section>
+  );
+};
+
+export default EmptyView;
